223d4587af12bf8eec910e52e7170740e6adb9b8
Ashley Dumaine <ashley.dumaine@gmail.com>
2018-02-18 16:48:26 -0500

    split out ratings function, add working Morningstar rating function

--- investogator_cli/investogator_cli.py
+++ investogator_cli/investogator_cli.py
@@ -17,12 +17,16 @@ def get_ratings(symbol):
     if symbol is None:
         print("No symbol supplied!")
         return
-    zacks_op = check_rating("Zacks", symbol)
-    if zacks_op is None:
-        print("Can't reach Zacks at the moment...")
-    morningstar_op = check_rating("Morningstar", symbol)
-    if morningstar_op is None:
-        print("Can't reach Morningstar at the moment...")
+    zacks_rating = check_zacks_rating(symbol)
+    if zacks_rating is None:
+        print("Can't reach Zacks at the moment.")
+    ms_rating = check_ms_rating(symbol)
+    if ms_rating is None:
+        print("Can't reach Morningstar at the moment.")
+    elif ms_rating == -1:
+        print("{0} doesn't seem to have a rating.".format(symbol))
+    else:
+        print("Morningstar rates {0} at a {1} out of 5".format(symbol, ms_rating))
     return
 
 @cli.command("get-sustainability")
@@ -38,21 +42,16 @@ def get_sustainability(symbol):
     print("Morningstar reports {0} sustainability for {1}.".format(sus_rating, symbol))
     return
 
-def check_rating(site, symbol):
-    if site == "Zacks":
-        page = requests.get()
-    elif site == "Morningstar":
-        page = requests.get("http://www.morningstar.com/etfs/arcx/" + symbol + "/quote.html")
-    else:
-        logger.debug("Unrecognized site {0}".format(site))
-        return None
+def check_zacks_rating(symbol):
+    # TODO: implement this
+    return None
+
+def check_ms_rating(symbol):
+    page = requests.get("http://www.morningstar.com/api/v1/security-identifier/" + symbol)
     if page.status_code != 200:
-        logger.debug("{0} is returning status code {1}".format(site, page.status_code))
+        logger.debug("Morningstar is returning status code {1}".format(page.status_code))
         return None
-    # We use Beautiful Soup here since it doesn't seem there's an API to use for Morningstar
-    soup = BeautifulSoup(page.text, 'html.parser')
-    # TODO: implement the rest of this...
-    return
+    return page.json()['starRating']
 
 def check_ms_sustainability(symbol):
     page = requests.get("http://etfs.morningstar.com/etfq/esg-etf?&t=ARCX:" + symbol + "&region=usa&culture=en-US&version=RET&cur=&test=QuoteiFrame&e=eyJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.tSDHKSXdLz9Z-95RIt28jRBS2bycBEfA83K4AAWtpDsmwZ3eZJuOatSWRjLITgGJVDutGigpcxJx7ojDFi4SiUq93kz5skXbVz9We3sUV5xXKSDW7W5HTtV0Oh4eSYl4bjf_csXP5pexU-eAYvofcSrHtmEn_GOcQ4GJA_qzXJc.70cJ7G9sm6MH4gYA.eZUC9ESEz_SGFNEd4tF5O3eXrS6mKs8-AWDZH4r55aD1Bm0K2R-Na5bOxUmXr2XbGTrkfbi--UTrPjvwRxAWYbMIGxs12mKL1etKxdmVImSVhN2bk4WDOfV9Vnotfu0-YigkPJHAAPnnB5owMwmWOmSsm-Xxxo-kFLK5C1K3YYBYe2ruwZVxO1rJ6se-ruzjipYAmRR9vIjxaUvadr0bAOxnS8KbgFIdv5fBILs.nEgzKr6eTtw896WO8yE1mA&_=1518983228036")
